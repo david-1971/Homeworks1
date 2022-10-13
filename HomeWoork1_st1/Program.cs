b4a27c22815976cd5b083115d42e6fea9ee2732e
David <ladbl1971@gmail.com>
2022-10-13 16:16:22 +0400

    first visual studio push

---
+++ HomeWoork1_st1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWoork1_st1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("David Kstsadze!");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(Console.ReadLine());
+
+        }
+    }
+}
